eddf6c4d443fa466547aeeb25a8fe05d8b1efdba
Nuccio Patrik <nucciopatriksch@gmail.com>
2022-10-12 17:43:22 +0200

    Balanced Bully crit damage value to 12%.

--- modules/cloth_mods/all_cloth.lua
+++ modules/cloth_mods/all_cloth.lua
@@ -7,8 +7,17 @@
 
 -- EVERY CLOTHES
 
-	-- BULLY Legendary (balanced stat crit damage to 20%)
-	TweakDB:SetFlat("Items.SimpleFabricEnhancer04_inline0.value", 20)
-	TweakDB:SetFlat("Items.SimpleFabricEnhancer04_inline2.value", 20)
-	TweakDB:SetFlat("Items.SimpleFabricEnhancer04.placementSlots", {"AttachmentSlots.FaceFabricEnhancer1", "AttachmentSlots.HeadFabricEnhancer1", "AttachmentSlots.FootFabricEnhancer1", "AttachmentSlots.LegsFabricEnhancer1", "AttachmentSlots.LegsFabricEnhancer2", "AttachmentSlots.InnerChestFabricEnhancer1", "AttachmentSlots.InnerChestFabricEnhancer2", "AttachmentSlots.OuterChestFabricEnhancer1", "AttachmentSlots.OuterChestFabricEnhancer2"})
+	-- BULLY Legendary (balanced stat crit damage to 12%)
+	local bullySlots = {'AttachmentSlots.FaceFabricEnhancer1',
+						'AttachmentSlots.HeadFabricEnhancer1',
+						'AttachmentSlots.FootFabricEnhancer1', 
+						'AttachmentSlots.LegsFabricEnhancer1', 
+						'AttachmentSlots.LegsFabricEnhancer2', 
+						'AttachmentSlots.InnerChestFabricEnhancer1',
+						'AttachmentSlots.InnerChestFabricEnhancer2',
+						'AttachmentSlots.OuterChestFabricEnhancer1',
+						'AttachmentSlots.OuterChestFabricEnhancer2'}
+	TweakDB:SetFlat("Items.SimpleFabricEnhancer04_inline0.value", 12)
+	TweakDB:SetFlat("Items.SimpleFabricEnhancer04_inline2.value", 12)
+	TweakDB:SetFlat("Items.SimpleFabricEnhancer04.placementSlots", bullySlots)
 	TweakDB:SetFlat("Items.SimpleFabricEnhancer04.itemType", "ItemType.Prt_FabricEnhancer")
